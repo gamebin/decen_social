4c78ed34aea2f9481e62c68b037cbc4f80a22bf9
stub <stub@gamebin.co.kr>
2019-11-29 14:54:23 +0900

    Add new function definition

--- api/server/auth/auth.route.js
+++ api/server/auth/auth.route.js
@@ -1,24 +1,26 @@
-const express = require('express');
-const validate = require('express-validation');
-const expressJwt = require('express-jwt');
-const paramValidation = require('../../config/param-validation');
-const authCtrl = require('./auth.controller');
-const config = require('../../config/config');
-const userCtrl = require('../user/user.controller');
+const express = require("express");
+const validate = require("express-validation");
+const expressJwt = require("express-jwt");
+const paramValidation = require("../../config/param-validation");
+const authCtrl = require("./auth.controller");
+const config = require("../../config/config");
+const userCtrl = require("../user/user.controller");
 
 const router = express.Router(); // eslint-disable-line new-cap
 
 /** POST /api/auth/login - Returns token if correct userid and userpasswd is provided */
-router.route('/login')
-  .post(validate(paramValidation.login), authCtrl.login);
+router.route("/login").post(validate(paramValidation.login), authCtrl.login);
 
 /** POST /api/auth/signup - Returns ok if correct userid and userpasswd is provided */
-router.route('/signup')
+router
+  .route("/signup")
   .post(validate(paramValidation.createUser), userCtrl.create);
 
 /** GET /api/auth/random-number - Protected route,
-* needs token returned by the above as header. Authorization: Bearer {token} */
-router.route('/random-number')
+ * needs token returned by the above as header. Authorization: Bearer {token} */
+router
+  .route("/random-number")
   .get(expressJwt({ secret: config.jwtSecret }), authCtrl.getRandomNumber);
 
+/** POST /api/auth/logout - TODO need to implement */
 module.exports = router;
